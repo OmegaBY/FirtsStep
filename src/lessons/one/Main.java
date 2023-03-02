d356ffe53f8e106e5b596e771b036458bc03afa5
Aliaksei Samborskiy <samborskiyalex@gmail.com>
2023-03-03 00:14:15 +0300

    02- check commit

--- src/lessons/one/Main.java
+++ src/lessons/one/Main.java
@@ -6,6 +6,8 @@ public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat();
         cat.sleep();
+
+        String badcommit = "not very bad";
         // Чо опять
 
 
